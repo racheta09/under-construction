e6a019c035be2c81f711e3c8f3d5a1e71645c65a
racheta <surajshogun@gmail.com>
2023-06-09 05:32:28 +0530

    rebranding update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 import Image from "next/image"
+import Link from "next/link"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[url(/construction.gif)] bg-cover">
-      <h1 className="text-6xl text-center">Website is under contruction</h1>
+      <h1 className="text-6xl text-center font-bold">
+        We are rebranding from BabyCasper to{" "}
+        <Link href="https://pepe2earn.com">Pepe2Earn</Link>
+      </h1>
+      <p className="text-3xl">
+        {" "}
+        For more info contact us on{" "}
+        <Link href="mailto:admin@pepe2earn.com">admin@pepe2earn.com</Link>
+      </p>
     </main>
   )
 }
