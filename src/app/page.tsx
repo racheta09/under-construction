48b17d59046efeb1cc43db1d9d08445abc1d7dcf
racheta <surajshogun@gmail.com>
2023-06-19 18:08:05 +0530

    update link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[url(/construction.gif)] bg-cover">
       <h1 className="text-6xl text-center font-bold">
-        We are rebranding from BabyCasper to{" "}
-        <Link href="https://pepe2earn.com">Pepe2Earn</Link>
+        Website is under Construction
       </h1>
       <p className="text-3xl">
-        {" "}
         For more info contact us on{" "}
-        <Link href="mailto:admin@pepe2earn.com">admin@pepe2earn.com</Link>
+        <Link href="mailto:admin@bscgrand.exchange">
+          admin@bscgrand.exchange
+        </Link>
       </p>
     </main>
   )
