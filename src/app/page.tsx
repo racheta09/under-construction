88b20c4155d65b3fc8e5564e106ead7300613335
racheta <surajshogun@gmail.com>
2023-07-15 10:18:21 +0530

    dogereversed update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,25 @@ import Link from "next/link"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[url(/construction.gif)] bg-cover">
-      <h1 className="text-6xl text-center font-bold">
-        Website is under Construction
-      </h1>
-      <p className="text-3xl">
-        For more info contact us on{" "}
-        <Link href="mailto:admin@bscgrand.exchange">
-          admin@bscgrand.exchange
-        </Link>
-      </p>
+    <main className="hero min-h-screen bg-[url(/construction.gif)] bg-cover ">
+      <div className="hero-overlay bg-opacity-40"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold text-slate-950">
+            Website is under Construction
+          </h1>
+          <p className="mb-5 text-3xl text-slate-950">
+            For more info contact us on{" "}
+            <Link href="mailto:admin@bscgrand.exchange">
+              admin@dogereversed.lol
+            </Link>
+            <br />
+            <Link href="https://twitter.com/dogereversed">
+              Twitter @dogereversed
+            </Link>
+          </p>
+        </div>
+      </div>
     </main>
   )
 }
